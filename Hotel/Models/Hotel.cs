cd2c9125451e600a1052c316d309e56b4528447c
Petar <pera.raickovic@gmail.com>
2021-01-23 17:35:57 +0100

    Initial add of the files

---
+++ Hotel/Models/Hotel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelProgam.Models
+{
+    public class Hotel
+    {
+
+        public List<Room> rooms { get; set; }
+
+
+        public Hotel(int n)
+        {
+            rooms = new List<Room>();
+            for(var i = 1; i<=n; i++)
+            {
+                Room newRoom = new Room
+                {
+                    roomNumber = i,
+                    occupancies = new List<ReservationDates>()
+                };
+                rooms.Add(newRoom);
+            }
+        }
+    }
+}
---
+++ Hotel/Models/ReservationDates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelProgam.Models
+{
+    public class ReservationDates
+    {
+        public int from { get; set; }
+
+        public int to { get; set; }
+
+
+
+    }
+}
